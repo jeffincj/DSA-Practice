2befa276352301308c9ac09940cc9f190ccf5190
jeffincj <cj.jeffincjohn@gmail.com>
2026-01-24 00:34:30 +0530

    Roman to Integer
    Implement Roman to Integer conversion logic.

---
+++ 0013-Roman-to-Integer.py
@@ -0,0 +1,14 @@
+class solution:
+  def romanToInt(self,s: str) ->int:
+    roman_map = { 'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M': 1000}
+    total = 0;
+    for i in range(len(s)):
+      #Check if the current value is less than the next value
+      if i+1 < len(s) and roman_map[s[i]] < roman_map[s[i+1]]:
+        total = total -roman_map[s[i]]
+      else:
+        total += roman_map[s[i+1]]
+    return total
+# To run it:
+sol = Solution()
+print(sol.romanTototal("MCMXCIV")) #1994
