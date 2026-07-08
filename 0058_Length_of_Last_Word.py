e46453912e8f1a5261a9df212d64c852d9226337
Jeffin C John <cj.jeffincjohn@gmail.com>
2026-07-09 00:36:18 +0530

    Delete 0058_Length_of_Last_Word.py

--- 0058_Length_of_Last_Word.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        s = s.rstrip()
-        words = s.split()
-        if not words:
-            return 0
-        return len(words[-1])
