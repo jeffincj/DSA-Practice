42b0e10175d7896a6559bc6a5e5576c2511a073c
Jeffin C John <cj.jeffincjohn@gmail.com>
2026-07-09 00:34:03 +0530

    Delete 0027_remove_element.py

--- 0027_remove_element.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        expected =[]
-        a=0
-        for n in range(len(nums)):
-            if nums[n]!=val :
-                nums[a]=nums[n]
-                a=a+1
-        return a
